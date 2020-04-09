2ef3ccf8711a95dcf73b0867727e621dcb8965c8
oranye <n.oranye@genesystechhub.com>
2020-04-09 11:41:15 +0100

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.teal,
+                  child: Text(''),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
